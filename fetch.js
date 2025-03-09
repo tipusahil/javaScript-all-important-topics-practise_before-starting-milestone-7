2f17db3e64fa8e9ccffd8ca965f933dd9a5763b0
tipusahil <tipusahil.ctg@gmail.com>
2025-03-09 12:17:08 +0600

    fetch related all

--- fetch.js
+++ fetch.js
@@ -4,8 +4,14 @@ const student1 = {
     moives:["king khan" ,"dhakar mastan"],
 }
 
-// ----JSON.stringify er maddome obj k strigified kora----------------------------
+// 1. ----JSON.stringify er maddome obj k strigified kora----------------------------
  const strigified = JSON.stringify(student1);
 
  console.log(strigified)//output:
  //  {"name_a":"sakib khan","age3":80,"moives":["king khan","dhakar mastan"]}
+
+ 
+// 2. ----JSON.parse er maddome strigified kora obj k abr direct obj er rop e firai ana ----------------------------
+
+const strigified_to_obj = JSON.parse(strigified);
+console.log(strigified_to_obj)
