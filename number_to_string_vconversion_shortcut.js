23d2db57ee00438b13621167aad3ccb3547b1eda
tipusahil <tipusahil.ctg@gmail.com>
2025-03-09 17:07:46 +0600

    ternary advance done

--- number_to_string_vconversion_shortcut.js
+++ number_to_string_vconversion_shortcut.js
@@ -7,4 +7,7 @@ let y = x + '';// x varaible er sate  mt_string jog/concat korlei sei x er vlaue
 console.log(typeof y)
 
 
-// --------------------
+// --------------------number data string to number e convert shorthand way---
+
+let input = '453';
+const afterStringToNumber = +input;// oi string value wala variable er age (+)  dile sei string value ta number e convert hoye jai.
