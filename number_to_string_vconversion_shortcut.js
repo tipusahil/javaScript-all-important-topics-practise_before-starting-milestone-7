eff090381f2ebde3c3d2d4b9f448bff7fc1393a0
tipusahil <tipusahil.ctg@gmail.com>
2025-03-09 21:25:54 +0600

    storage done

--- number_to_string_vconversion_shortcut.js
+++ number_to_string_vconversion_shortcut.js
@@ -1,3 +1,8 @@
+// toggle boolean koorar jonno:
+let isActive1 = true;
+isActive1 = !isActive1;// true thakle false korbe ar false thakle true korbe, ortat (togggle) korbe
+
+
 
 // ----------------number to string conversion shortcut:--------------------
 let x = 10;
@@ -11,3 +16,17 @@ console.log(typeof y)
 
 let input = '453';
 const afterStringToNumber = +input;// oi string value wala variable er age (+)  dile sei string value ta number e convert hoye jai.
+
+const isActive = tr ;
+const show_User = () => console.log("display user , true");
+const hide_User = () => console.log("hide user ,false");
+
+isActive ? show_User() : hide_User();
+
+// -----------------------------------aro shortcut :
+// use (&&) if left side  is true then right side will be executed;
+// ortat niser condtion ta holo: (isActive = true) hole (&&) er right side ta execute hbe ortat run hobe ,ar jodi left side false hoi thle right side execute hobena.
+isActive && show_User();
+
+
+isActive || hide_User();// left side flase hole right side execute hboe.ortat (&&) er shortcut tar biporid kajta korbe.
