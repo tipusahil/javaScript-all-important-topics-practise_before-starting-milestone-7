cbe8dad40b8c250aa1c0fa58546c3df553f0a641
tipusahil <tipusahil.ctg@gmail.com>
2025-03-09 15:53:45 +0600

    shortcut file created

--- ES6.js
+++ ES6.js
@@ -1,29 +1,3 @@
-const student1 = {
-    name_a : "sakib khan",
-    age3:80,
-    moives:["king khan" ,"dhakar mastan"],
-}
-
-// 1. ----JSON.stringify er maddome obj k strigified kora----------------------------
- const strigified = JSON.stringify(student1);
-
- console.log(strigified)//output:
- //  {"name_a":"sakib khan","age3":80,"moives":["king khan","dhakar mastan"]}
-
- 
-// 2. ----JSON.parse er maddome strigified kora obj k abr direct obj er rop e firai ana ----------------------------
-
-// const strigified_to_obj = JSON.parse(strigified);
-// console.log(strigified_to_obj);
-
-
-// json fetch api niyee kaj kora:
-
-// fetch("url")
-// .then(res => res.json())
-// .then(data => console.log(data))
-// .catch(error => console.log(error))
-
 
 // objeect er keys & values & entries/pairs eksatee newa :
 const keys =Object.keys(student1);
